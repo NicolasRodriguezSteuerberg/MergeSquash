0e731f66bf7f780678a3d70b4e0781ed9c7685dd
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-01-18 14:29:17 +0100

    merge rama colaborador

---
+++ src/Colaborador.java
@@ -0,0 +1,7 @@
+public class Colaborador {
+    public void saludo(){
+        System.out.println("1 gabriel");
+        System.out.println("2 gabriel");
+        System.out.println("3 gabriel");
+    }
+}
