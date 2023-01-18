b807637c7bb589d61825589c980252887d972b1e
Gabriel Perez <gperezpereira@danielcastelao.org>
2023-01-18 14:19:24 +0100

    3 commit gabriel

--- src/Colaborador.java
+++ src/Colaborador.java
@@ -2,5 +2,6 @@ public class Colaborador {
     public void saludo(){
         System.out.println("1 gabriel");
         System.out.println("2 gabriel");
+        System.out.println("3 gabriel");
     }
 }
